88a59b340aaecba21d94e29e525b8f0927e88c79
Nitakaleo <85638480+Nitakaleo@users.noreply.github.com>
2021-10-11 11:12:05 +0200

    Add files via upload

---
+++ network/data_preprocessing.py
@@ -0,0 +1,122 @@
+import os
+
+import nltk
+
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.utils import shuffle
+import pickle
+from network.utils import pos_tag_map
+
+from nltk.stem import WordNetLemmatizer
+from nltk import pos_tag, word_tokenize
+
+lemmatizer = WordNetLemmatizer()
+
+"""preprocessing in extracted words: lower case, lemmatization, remove duplicate, remove punctuation mark"""
+
+
+def lemmatize_words(words_sequence):
+    return [lemmatizer.lemmatize(w.lower(), pos_tag_map[p]) if pos_tag_map[p] is not None and w.isalpha()
+            else lemmatizer.lemmatize(w.lower()) for w, p in pos_tag(words_sequence) if w.isalpha()]
+
+
+def lemmatize_and_preprocess_words(words_list):
+    """Filter punctuations marks, make in lower case and then lemmatize"""
+
+    lemmatized_words = lemmatize_words(words_list)
+
+    """remove duplicates"""
+    return sorted(list(set(lemmatized_words)))
+
+
+"""
+load the dataset, intents.json and make preprocessing on data
+like tokenization 
+"""
+
+
+def pattern_to_BoW(words_list_lemmatized, pattern, tokenized=True):
+    BoW = []
+    if not tokenized:
+        pattern = word_tokenize(pattern)
+        pattern = lemmatize_words(pattern)
+
+        print("\n\nPATTERN: ", pattern, "\n\n")
+
+    for w in words_list_lemmatized:
+        BoW.append(1) if w in pattern else BoW.append(0)
+
+    return BoW
+
+
+def load_and_preprocess_data(intents):
+    patterns_lemmatized = []
+    words_list = []
+    classes = []
+    labels = []
+    responses = []
+    for intent in intents['intents']:
+        for pattern in intent['patterns']:
+            # use tokenization to extract words from a given pattern
+            extracted_words = word_tokenize(pattern)
+            # build list of the words
+            words_list.extend(extracted_words)
+            # save couple pattern and corresponding labels
+            patterns_lemmatized.append(lemmatize_words(extracted_words))
+            labels.append(intent['tag'])
+
+        responses.append(intent['responses'])
+
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+    classes = sorted(list(classes))
+    words_list_lemmatized = lemmatize_and_preprocess_words(words_list)
+
+    print("\nLEMMATIZED WORDS: ", words_list_lemmatized, "\n")
+
+    if not os.path.isdir("model"):
+        os.mkdir("model")
+
+    pickle.dump(words_list_lemmatized, open('model/words_list_lemmatized.pickle', 'wb'))
+    pickle.dump(classes, open('model/classes.pickle', 'wb'))
+
+    return words_list_lemmatized, classes, patterns_lemmatized, labels
+
+
+def get_train_and_test(json_intents_filename):
+    # get preprocessed data
+
+    words_list_lemmatized, classes, pattern_lemmatized, labels = load_and_preprocess_data(json_intents_filename)
+
+    # create x_train and y_train
+    x_train = []
+    y_train = []
+
+    # space for a row labels
+    encoded_labels = np.zeros(len(classes))
+
+    # training set, bag of words for each sentence
+    for pattern, label in zip(pattern_lemmatized, labels):
+        # create a bags of words
+        BoW = pattern_to_BoW(words_list_lemmatized, pattern)
+
+        x_train.append(BoW)
+        # output is a '0' for each tag and '1' for current tag (for each pattern)
+        aux_lab = list(encoded_labels)
+
+        aux_lab[classes.index(label)] = 1
+
+        y_train.append(aux_lab)
+
+    x_train = np.array(x_train)
+    y_train = np.array(y_train)
+    # shuffle data
+    x_train, y_train = shuffle(x_train, y_train)
+    # create train and test lists. X - patterns, Y - intents
+    print("X_train, Y_train created correctly")
+
+    print("X_train: ", x_train.shape)
+    print("Y_train: ", y_train.shape)
+
+    return x_train, y_train
---
+++ network/network.py
@@ -0,0 +1,88 @@
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import Dense, Activation, Dropout, Embedding, GlobalAveragePooling1D
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.utils import plot_model
+from network.data_preprocessing import pattern_to_BoW
+from network.utils import Bcolors
+import numpy as np
+import pickle
+import os
+
+
+# TODO: se c'è tempo provare con RNN o LSTM
+class ChatbotDNN:
+
+    def __init__(self, x_train=None, y_train=None):
+
+        if os.path.isfile("model/model.h5") and x_train is None and y_train is None:
+            self.model = load_model("model/model.h5")
+            self.loaded = True
+            print(Bcolors.OKBLUE + "\nModel loaded Correctly." + Bcolors.ENDC)
+        else:
+            print(Bcolors.OKCYAN + "Model doesn't exist, it will be created ..." + Bcolors.ENDC)
+            self.x_train = x_train
+            self.y_train = y_train
+
+            with open('model/classes.pickle', 'rb') as file:
+                classes = pickle.load(file)
+
+            self.model = Sequential()
+            self.model.add(Dense(128, input_shape=(x_train.shape[1],), activation='relu'))
+            self.model.add(Dense(64, activation='relu'))
+            self.model.add(Dense(32, activation='relu'))
+            self.model.add(Dense(16, activation='relu'))
+
+            self.model.add(Dense(len(classes), activation="softmax"))
+            # set the optimizer
+            sgd = SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+            # compile the model
+            self.model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+            self.model.summary()
+            self.loaded = False
+
+    # Train the model if it doesn't exists, otherwise is loaded
+    def fit(self):
+
+        if not self.loaded:
+            print(Bcolors.OKBLUE + "\nModel not exist, starting training...\n" + Bcolors.ENDC)
+
+            early_stop = EarlyStopping(monitor='loss',
+                                       verbose=1, mode='min')
+
+            # Train the model
+            hist = self.model.fit(self.x_train, self.y_train, epochs=200, batch_size=16, verbose=1)
+            print(Bcolors.OKBLUE + "\nSaving model ...\n" + Bcolors.ENDC)
+
+            if not os.path.isdir("model"):
+                os.mkdir("model")
+            self.model.save('model/model.h5', hist)
+            ''' Uncomment print_model ONLY if is installed pydot and graphviz '''
+            # plot_model(self.model, to_file="model/model.png")
+            print(Bcolors.OKBLUE + "\nModel saved correctly.\n" + Bcolors.ENDC)
+
+    def predict(self, new_pattern):
+
+        with open('model/words_list_lemmatized.pickle', 'rb') as file:
+            words_list_lemmatized = pickle.load(file)
+
+        print("\nVOCABULARY DIMENSION: ", len(words_list_lemmatized), "\n")
+
+        with open('model/classes.pickle', 'rb') as file:
+            classes = pickle.load(file)
+
+        # preprocessing of the new pattern
+        BoW = pattern_to_BoW(words_list_lemmatized, new_pattern, tokenized=False)
+        print("BOW: ", BoW)
+        prediction = self.model.predict(np.array([BoW]))[0]
+        print("\n\n------Class Index Prediction: -------", np.argmax(prediction))
+
+        print("CLASSES: ", [(i, float(j)) for i, j in zip(classes, prediction)])
+
+        print("(\n(Predicted Class , Probability): ", classes[np.argmax(prediction)], max(prediction))
+
+        probability = max(prediction)
+        if probability >= 0.75:
+            return classes[np.argmax(prediction)]
+        else:
+            return "no_answer"
